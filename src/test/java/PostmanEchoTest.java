623146ab925a069964efe560805d94d05f857d89
boshih <artem.bolshih@mail.ru>
2022-08-03 22:59:48 +0300

    start

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+  @Test
+  void PostmanEchoTest() {
+    given()
+            .baseUri("https://postman-echo.com")
+            .body("kakito dannye") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+            .when()
+            .post("/post")
+// Проверки
+            .then()
+            .statusCode(200)
+            .body("data", equalTo("kakito dannye"))
+    ;
+  }
+}
